cef176526cb1ed6f5cab20ca08488c06b22252f5
mh-lornejad <132081911+mh-lornejad@users.noreply.github.com>
2024-04-20 15:27:43 +0330

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-    int x = 5;
-    std::cout << "The value of x is: " << x << std::endl;
+    int x;
+    std::cout << "The value of x is: " << x << std::endl; // Using uninitialized variable x
     return 0;
 }
 
