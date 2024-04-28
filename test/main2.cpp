9f0889c32e088ff70e9f1349dca996176019e5ea
mh-lornejad <132081911+mh-lornejad@users.noreply.github.com>
2024-04-28 16:35:24 +0330

    Update main2.cpp

--- test/main2.cpp
+++ test/main2.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5;
-    int b = 10;
-    int sum = a + b;
-    printf("Sum: %d\n", sum);
-    return 0;
+  int a = 5;
+  int b = 10;
+  int sum = a + b;
+  printf("Sum: %d\n", sum);
+  return 0;
 }
